79ad58ce30ca00f49a28d96bdf2f6df5d459291b
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2021-07-27 23:13:02 +1000

    first commit

--- sketch.js
+++ sketch.js
@@ -11,26 +11,26 @@ function setup() {
   let documentBody = select("body");
   let pInput = createP("Meme Caption: ");
   inputBox = createInput("Meme Caption");
-  inputBox.size(200, 30)
+  inputBox.size(200, 30);
   inputBox.parent(pInput);
   let memeUrlInput = createP("Image URL: ");
   imgUrlInput = createInput("https://.png");
-  imgUrlInput.size(200, 30)
+  imgUrlInput.size(200, 30);
   imgUrlInput.parent(memeUrlInput);
   let imgUrlSubmit = createButton("Upload Image");
   imgUrlSubmit.parent(memeUrlInput);
   imgUrlSubmit.mousePressed(() => {
     try {
-    img = loadImage(imgUrlInput.value());
-    } catch(e) {
+      img = loadImage(imgUrlInput.value());
+    } catch (e) {
       return e;
     }
-  })
+  });
   let pFont = createP("Font Size: ");
-  fontSizeSlider = createSlider(10, 100, 55,2);
+  fontSizeSlider = createSlider(10, 100, 55, 2);
   fontSizeSlider.parent(pFont);
-  memeName = createInput("MemeName.png")
-  memeName.size(130, 20)
+  memeName = createInput("MemeName.png");
+  memeName.size(130, 20);
   saveBtn = createButton("Save Meme");
   saveBtn.mouseClicked(saveMeme);
   documentBody.drop(droppedFile);
@@ -39,22 +39,22 @@ function setup() {
 function draw() {
   background(0);
   fill(255);
-  rect(0, 0, width, 55)
-  if(img) {
-    img.resize(width,height-55)
-    image(img, 0,55);
+  rect(0, 0, width, 55);
+  if (img) {
+    img.resize(width, height - 55);
+    image(img, 0, 55);
   } else {
     push();
     fill(255);
     textSize(80);
     textAlign(CENTER);
-    text("Drag image here!",0, (height-55)/3, width, height-55);
+    text("Drag image here or Paste Image URL below!", 0, (height - 55) / 3, width, height - 55);
     pop();
   }
   fill(0);
   textSize(fontSizeSlider.value());
   textAlign(CENTER);
-  text(inputBox.value(), 0,0, width, 55);
+  text(inputBox.value(), 0, 0, width, 55);
 }
 
 function droppedFile(file) {
@@ -62,6 +62,9 @@ function droppedFile(file) {
 }
 
 function saveMeme() {
-  saveCanvas(canvas, memeName.value().split(".")[0], memeName.value().split(".")[1] || "png");
+  saveCanvas(
+    canvas,
+    memeName.value().split(".")[0],
+    memeName.value().split(".")[1] || "png"
+  );
 }
-
