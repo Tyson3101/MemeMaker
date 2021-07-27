10f4fcfcb00d2ef36685f40dec67dbf9111c96b6
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2021-07-28 00:43:21 +1000

    Update Files

--- sketch.js
+++ sketch.js
@@ -11,64 +11,52 @@ function setup() {
   let documentBody = select("body");
   let pInput = createP("Meme Caption: ");
   inputBox = createInput("Meme Caption");
-  inputBox.size(200, 30);
+  inputBox.size(200, 30)
   inputBox.parent(pInput);
   let memeUrlInput = createP("Image URL: ");
   imgUrlInput = createInput("https://.png");
-  imgUrlInput.size(200, 30);
+  imgUrlInput.size(130, 20)
   imgUrlInput.parent(memeUrlInput);
-  let imgUrlSubmit = createButton("Upload Image");
-  imgUrlSubmit.parent(memeUrlInput);
+  let uploadImgBtn = createButton("Upload Image");
+  uploadImgBtn.mouseClicked(() => {
+    img = loadImage(imgUrlInput.value());
+  });
+  uploadImgBtn.parent(memeUrlInput);
   let pFont = createP("Font Size: ");
-  fontSizeSlider = createSlider(10, 100, 55, 2);
+  fontSizeSlider = createSlider(10, 100, 55,2);
   fontSizeSlider.parent(pFont);
-  memeName = createInput("MemeName.png");
+  memeName = createInput("MemeName.png")
   memeName.size(130, 20);
   saveBtn = createButton("Save Meme");
-  imgUrlSubmit.mouseClicked(() => {
-    try {
-      img = loadImage(imgUrlInput.value());
-    } catch (e) {
-      return e;
-    }
-  });
   saveBtn.mouseClicked(saveMeme);
-  documentBody.drop((file) => {
-    img = loadImage(file.data);
-  });
+  documentBody.drop(droppedFile);
 }
 
 function draw() {
   background(0);
   fill(255);
   rect(0, 0, width, 55);
-  if (img) {
-    img.resize(width, height - 55);
-    image(img, 0, 55);
+  if(img) {
+    img.resize(width,height-55);
+    image(img, 0,55);
   } else {
     push();
     fill(255);
     textSize(50);
     textAlign(CENTER);
-    text(
-      "Drag image here or Paste Image URL below!",
-      0,
-      (height - 55) / 3,
-      width,
-      height - 55
-    );
+    text("Drag image here or Paste Image URL below!",0, (height-55)/3, width, height-55);
     pop();
   }
   fill(0);
   textSize(fontSizeSlider.value());
   textAlign(CENTER);
-  text(inputBox.value(), 0, 0, width, 55);
+  text(inputBox.value(), 0,0, width, 55);
+}
+
+function droppedFile(file) {
+  img = loadImage(file.data);
 }
 
 function saveMeme() {
-  saveCanvas(
-    canvas,
-    memeName.value().split(".")[0],
-    memeName.value().split(".")[1] || "png"
-  );
+  saveCanvas(canvas, memeName.value().split(".")[0], memeName.value().split(".")[1] || "png");
 }
